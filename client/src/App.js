5d10068354cbd05982927aed69e656069ca65669
Joel Martinez <joemartinez87@hotmail.com>
2021-08-19 18:47:15 -0700

    setting up folder/file structure

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: InMemoryCache
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
