8a513ecbbac8ae785fba7c73efb5c5dfe335793e
Addeeze <nicholas.veen@gmail.com>
2021-08-21 21:42:56 -0700

    Working on setting up proper schema. Wrote 2 options, see which works.

--- Backend/models/Adoption.js
+++ Backend/models/Adoption.js
@@ -1,11 +1,11 @@
 const { Schema, model } = require('mongoose');
 
-const techSchema = new Schema({
-  name: {
+const adoptionSchema = new Schema({
+  /*name: {
     type: String,
     required: true,
     unique: true,
-  },
+  },*/
   breed: {
     type: String,
     required: true,
@@ -18,6 +18,26 @@ const techSchema = new Schema({
   }
 });
 
-const Tech = model('Tech', techSchema);
+/*const adoptionList =[
+  {
+    id:0,
+    name
+  }
+]
+*/
+// Need to figure out which one will work. 
+// const Adoption = mongoose.model('Adoption', Adopt);
+const Adoption = model('Adoption', adoptionSchema);
 
-module.exports = Tech;
+module.exports = Adoption;
+
+// We need to make sure that this works in order to push to main.
+const Adopt = new Schema({
+  name: {type: String},
+  adoptionSchema: [
+    {
+      breed: {type: String, required: true, unique: false},
+      age: {type: Number, required: true, unique: false}
+    }
+  ]
+})
