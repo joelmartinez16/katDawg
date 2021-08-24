9c909438737721e2806ee34f3921f517ae2032dc
Amalec Guzman <amalec.c.guzman@gmail.com>
2021-08-24 14:06:19 -0700

    updates

---
+++ Backend/routes/api/user-routes.js
@@ -0,0 +1,15 @@
+//Creating the user routes 
+
+const express = require('express'); 
+const router = express.Router(); 
+
+router.post('/signing',(req,res)=>{ 
+
+});  
+
+router.post('/signup',(req,res)=>{ 
+
+}); 
+
+modelu.export = router;
+
