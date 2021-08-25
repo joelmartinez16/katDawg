90c54b30db7c7a3e77192053c95c09bba2050536
Addeeze <nicholas.veen@gmail.com>
2021-08-24 20:02:50 -0700

    comments

--- Backend/models/Adoption.js
+++ Backend/models/Adoption.js
@@ -32,6 +32,7 @@ const Adoption = model('Adoption', adoptionSchema);
 module.exports = Adoption;
 
 // We need to make sure that this works in order to push to main.
+/*
 const Adopt = new Schema({
   name: {type: String},
   adoptionSchema: [
@@ -41,3 +42,4 @@ const Adopt = new Schema({
     }
   ]
 })
+*/
