e33f150dc7c6872e86f45d1a2ab68b7706396fd3
SIN88 <KG19882020@GMAIL.COM>
2021-08-26 16:56:03 -0700

    changes to user.js in the back end

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,5 +1,7 @@
 const Tech = require('./Tech');
 const Matchup = require('./Matchup');
-const App = require("../../client/src/App")
+const App = require('../../client/src/App');
+const User = require ('./User');
+const Adoption = require('./Adoption');
 
-module.exports = { Tech, Matchup, App };
+module.exports = { Tech, Matchup, App, User ,Adoption};
