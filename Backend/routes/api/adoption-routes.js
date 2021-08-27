4c3199a75b112650c2723a94293ee8043c3b1ecc
Addeeze <nicholas.veen@gmail.com>
2021-08-26 20:40:12 -0700

    Adding instructions on adoption routes.

--- Backend/routes/api/adoption-routes.js
+++ Backend/routes/api/adoption-routes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-
+//pulling information from adoption controller to get all pets and get them by their ids
 const {getAllPets, getPetsbyID} = require("../../controllers/adoption")
 
 router.get("/",getAllPets);
